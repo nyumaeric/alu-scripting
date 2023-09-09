f554d6a1dc34223d501652bc90f7fc3649fc5459
nyumaeric <e.nyumajr@alustudent.com>
2023-09-09 08:14:38 -0700

    8-textme.rb

---
+++ regular_expressions/8-textme.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Extract sender, receiver, and flags using scan and regular expressions
+matches = ARGV[0].scan(/\[from:(.?)\] \[to:(.?)\] \[flags:(.*?)\]/)
+
+# Check if any matches were found
+if matches.any?
+  matches.each do |match|
+    sender = match[0]
+    receiver = match[1]
+    flags = match[2]
+    puts "#{sender},#{receiver},#{flags}"
+  end
+else
+  puts "No matches found."
+end
